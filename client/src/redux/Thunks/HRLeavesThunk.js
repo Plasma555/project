9f45c4609539908673f38705bd8bafccfcef54d6
plasma555 <asouk5@gmail.com>
2026-05-30 19:40:00 +0530

    Fix Linux case-sensitive imports

--- client/src/redux/Thunks/HRLeavesThunk.js
+++ client/src/redux/Thunks/HRLeavesThunk.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "../apis/apiService";
+import { apiService } from "../apis/APIService";
 import { HRLeavesPageEndPoints } from "../apis/APIsEndpoints";
 
 export const HandleGetHRLeaves = createAsyncThunk('HandleGetHRLeaves', async (leaveData, { rejectWithValue }) => {
--- client/src/redux/Thunks/HRSalariesThunk.js
+++ client/src/redux/Thunks/HRSalariesThunk.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "../apis/apiService";
+import { apiService } from "../apis/APIService";
 import { HRSalariesPageEndPoints } from "../apis/APIsEndpoints";
 
 export const HandleGetHRSalaries = createAsyncThunk('HandleGetHRSalaries', async (salaryData, { rejectWithValue }) => {
